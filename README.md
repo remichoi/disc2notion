608eba171bc1c1cc40ac48c5515c48307bd1c141
Theresa Eunhye Choi <maangoliebe@gmail.com>
2021-09-03 10:25:50 -0400

    test commit w README

--- README.md
+++ README.md
@@ -2,15 +2,19 @@
 
 ## Setup
 
+*TBA*
+
 ## Workflow
 Discord receives user input
 * asks standup questions every weekday morning
 * **TODO:** 
-    * add dynamic input for last seen date (handle weekends, OOOs, etc)
-    * fetch user display name
     * add reactions to skip / record OOO (in either case, document date range and reason)
-* *Next: create/update OKR progress*
+    * allow multiple users
+    * create/update OKR progress*
 
 Notion hosts auto-generated documentation
 * uploads daily progress updates onto page
-* *Next: create databases for OKRs and update progress in databases*
+* **TODO:**
+    * link collection for current OKR
+    * create OKR
+    * update progress in OKR
--- main.py
+++ main.py
@@ -16,14 +16,30 @@ TOKEN = auth.disc_token
 bot = commands.Bot(command_prefix='!', intents=intents)
 team = [god]
 
-
 def check(user):
     def inner_check(message):
-        return message.author == user
+        return message.author == user and message.content[0] != '!'
     return inner_check
 
 async def check_in(user):
-    previous_response = get_from_notion()
+    date, tasks = get_from_notion()
+
+    standup_questions[0] = standup_questions[0].format(user.name)
+
+    if date: # previous entry exists
+        # turn date (ex. 2021-08-18) into human friendly form
+        time_passed = (datetime.datetime.today() - datetime.datetime.strptime(date, '%Y-%m-%d')).days
+        if time_passed == 1:
+            date = 'yesterday'
+        elif time_passed <= 7:
+            date = datetime.datetime.strptime(date, '%Y-%m-%d').strftime('last %A')
+        else:
+            date = datetime.datetime.strptime(date, '%Y-%m-%d').strftime('%b %d')
+
+        tasks_string = "".join("- " + task + "\n" for task in tasks).strip()
+        standup_questions[1] = standup_questions[1].format(date, tasks_string)
+    else:
+        standup_questions[1] = standup_questions[1].split(" Y")[0]
     
     user_responses = []
     for question in standup_questions:
@@ -40,7 +56,7 @@ async def check_in(user):
 async def on_ready():
     for guild in bot.guilds:
         for member in guild.members:
-            if member.id == god and not member.user.bot:
+            if member.id == god and not member.bot:
                 await check_in(member)
 
 @bot.command()
